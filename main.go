7917888ac05a88f0443a8f0d075d84bf44d9b845
Greg <grzegorz.karol.jasinski@gmail.com>
2024-08-24 01:18:28 +0200

    experiment with maxprocs and wait group

--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"runtime"
 	"strconv"
+	"sync"
 	"towebp/lib"
 )
 
+func scheduleConversion(wg *sync.WaitGroup, inputPath string, quality float32, lossless bool) {
+	defer wg.Done()
+	conversion := lib.NewConversion(inputPath, quality, lossless)
+	outputPath, err := conversion.ToWebp()
+	if err != nil {
+		lib.LogConversionError(inputPath, err.Error())
+		return
+	}
+
+	lib.LogConverted(inputPath, outputPath)
+}
+
 func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
 	imagesList, count, err := lib.NewImagesList(".").Scan()
 	if err != nil {
 		lib.LogError("LogError scanning directory: " + err.Error())
@@ -13,14 +29,12 @@ func main() {
 
 	lib.LogInfo("Found: " + strconv.Itoa(count) + " images")
 
-	for _, inputPath := range imagesList {
-		conversion := lib.NewConversion(inputPath, 80, false)
-		outputPath, err := conversion.ToWebp()
-		if err != nil {
-			lib.LogConversionError(inputPath, err.Error())
-			continue
-		}
+	var wg sync.WaitGroup
 
-		lib.LogConverted(inputPath, outputPath)
+	for _, inputPath := range imagesList {
+		wg.Add(1)
+		go scheduleConversion(&wg, inputPath, 80, false)
 	}
+
+	wg.Wait()
 }
